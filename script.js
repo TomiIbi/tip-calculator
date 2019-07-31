c9b5470a1dedec8a5d224868071ddbd5cb4303ac
TomiIbi <51913809+TomiIbi@users.noreply.github.com>
2019-07-31 21:08:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-	document.getElementsById('container').onchange = function() {
-		var bill = Number(document.getElementsById('billtotal').value);
-		var tipPercent = document.getElementsById('tipinput').value;
-		var Split = document.getElementsById('splitinput').value;
+	document.getElementById('container').onchange = function() {
+		var bill = Number(document.getElementById('billtotal').value);
+		var tipPercent = document.getElementById('tipinput').value;
+		var Split = document.getElementById('splitinput').value;
 		var tipvalue = bill * (tipPercent/100);
 		var newbilleach = (bill + tipvalue) / Split;
 		var tipeach = tipvalue / Split;
 
-		document.getElementsById('tipoutput').innerHTML = tipPercent + "%"; 
-		document.getElementsById('splitoutput').innerHTML = Split; 
-		document.getElementsById('newbill').innerHTML = "$" + newbilleach.toFixed(2);
-		document.getElementsById('tipeach').innerHTML = "$" + tipeach.toFixed(2);
+		document.getElementById('tipoutput').innerHTML = tipPercent + "%"; 
+		document.getElementById('splitoutput').innerHTML = Split; 
+		document.getElementById('newbill').innerHTML = "$" + newbilleach.toFixed(2);
+		document.getElementById('tipeach').innerHTML = "$" + tipeach.toFixed(2);
 	}
